5ce833b57507bb8f11d4777e5988cb1c9d3afa16
lida <kurogiri.dabi@gmail.com>
2022-04-09 14:41:10 +0300

    init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.regex.Matcher;
+import  java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+            System.out.println("Введите ip-адрес:");
+            Scanner sc1 = new Scanner(System.in);
+            String ip = sc1.nextLine();
+            String input = ip;
+            boolean result = input.matches("^((25[0-5]|2[4]\\d|[01]?\\d\\d?)\\.){3}(25[0-5]|2[4]\\d|[01]?\\d\\d?)$");
+            if (result) {
+                System.out.println("Ok");
+            }
+            else {
+                System.out.println("Error");
+            }
+        }
+    }
